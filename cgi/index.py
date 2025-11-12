f0496313e83fd94ef4f9abeb03b25ae77d7c97ba
Alexander Shevtsov <alecsandr.unit@gmail.com>
2025-11-12 20:16:30 +0200

    Заменил список на таблицу

---
+++ cgi/index.py
@@ -0,0 +1,35 @@
+#!C:\Users\alecs\source\repos\Server-KN-P-222\.venv\Scripts\python.exe
+
+import codecs, os, sys
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+sys.stdin = codecs.getreader("utf-8")(sys.stdin.detach())
+
+rows = "".join(f"<tr><td>{k}</td><td>{v}</td></tr>" for k, v in os.environ.items())
+
+envs = f"""
+<table border="1" cellspacing="0" cellpadding="5">
+  <tr>
+    <th>Имя переменной</th>
+    <th>Значение</th>
+  </tr>
+  {rows}
+</table>
+"""
+
+html = f"""<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Переменные окружения</title>
+    <link rel="icon" href="/python.png" />
+</head>
+<body>
+    <h1>Переменные окружения (CGI)</h1>
+    {envs}
+</body>
+</html>"""
+
+print("Content-Type: text/html; charset=utf-8")
+print()
+print(html)
