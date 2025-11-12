b3f611cf0b9c56198a31babb4facf03562f29eec
Alexander Shevtsov <alecsandr.unit@gmail.com>
2025-11-12 20:19:01 +0200

    Реализовал сортировку таблицы

--- cgi/index.py
+++ cgi/index.py
@@ -4,7 +4,10 @@ import codecs, os, sys
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 sys.stdin = codecs.getreader("utf-8")(sys.stdin.detach())
 
-rows = "".join(f"<tr><td>{k}</td><td>{v}</td></tr>" for k, v in os.environ.items())
+rows = "".join(
+    f"<tr><td>{k}</td><td>{v}</td></tr>"
+    for k, v in sorted(os.environ.items())
+)
 
 envs = f"""
 <table border="1" cellspacing="0" cellpadding="5">
